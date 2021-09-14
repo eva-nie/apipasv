63a681e064bf8dd73e1a423f36aef18d9e903baa
EvaN <vl1vl@yahoo.com>
2021-09-15 00:01:56 +0200

    add auth helper

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,44 +1,34 @@
 import {expect} from 'chai';
-import supertest from 'supertest';
+import AuthHelper from '../helpers/auth.helper';
 
 describe('Auth', function () {
-    let result;
+    let authHelper = new AuthHelper();
 
     describe('Successful log in', function () {
         before(async function () {
-            await supertest(process.env.BASE_URL)
-                .post('/auth')
-                .send({login: process.env.LOGIN, password: process.env.PASSWORD})
-                .then(res => {
-                    result = res;
-                });
+            await authHelper.get(process.env.LOGIN, process.env.PASSWORD);
         });
 
         it('Response status code is 200', function () {
-            expect(result.statusCode).to.eq(200);
+            expect(authHelper.response.statusCode).to.eq(200);
         });
 
         it('Response body contains authorization token', function () {
-            expect(result.body.token).not.to.be.undefined;
+            expect(authHelper.response.body.token).not.to.be.undefined;
         });
     });
 
     describe('Log in with wrong credentials should return error', function () {
         before(async function () {
-            await supertest(process.env.BASE_URL)
-                .post('/auth')
-                .send({login: 'wrong', password: 'wrong'})
-                .then(res => {
-                    result = res;
-                });
+            await authHelper.get( 'invalid', 'invalid');
         });
 
         it('Response status code is 404', function () {
-            expect(result.statusCode).to.eq(404);
+            expect(authHelper.response.statusCode).to.eq(404    );
         });
 
         it('Response status code is 404', function () {
-            expect(result.body.message).to.eq('Wrong login or password.');
+            expect(authHelper.response.body.message).to.eq('Wrong login or password.');
         });
     });
 });
