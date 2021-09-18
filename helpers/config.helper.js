577729a8ca7f4d36d4bfcdbef9f2c3401fd8bc35
EvaN <vl1vl@yahoo.com>
2021-09-19 00:27:51 +0200

    add config tests

--- helpers/config.helper.js
+++ helpers/config.helper.js
@@ -13,6 +13,28 @@ class ConfigHelper {
                 this.response = res;
             });
     }
-}
 
-export default ConfigHelper;
+    async getConfig() {
+        await supertest(process.env.BASE_URL)
+            .get('/config')
+            .set({Authorization: `Bearer ${process.env.TOKEN}`})
+            .then(res => {
+                this.response = res;
+            });
+    }
+
+        async patchConfig(users, amount) {
+            await supertest(process.env.BASE_URL)
+                .patch('/config')
+                .send({number_of_entries: users, initial_amount: amount})
+                .set({Authorization: `Bearer ${process.env.TOKEN}`})
+                .then(res => {
+                    this.response = res;
+                });
+        }
+    }
+
+
+    export
+    default
+    ConfigHelper;
